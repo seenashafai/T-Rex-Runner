6dba6b77864644d1dee2dd21f8180421aee04f89
seenashafai <seenas@btinternet.com>
2018-03-30 13:22:22 +0100

    Add Ground Detection & Eliminate Double Jump+

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Collections;
 
 public class Jump : MonoBehaviour {
+    
 	public bool grounded = true;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
 	public float jumpPower = 1;
 
-   	void Update () 
+	private void FixedUpdate()
+	{
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+
+	void Update () 
     {
         if (Input.GetButtonDown("Jump") && grounded == true) 
         {
